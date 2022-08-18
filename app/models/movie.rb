bd61235300413f29c6de9fcf99eba5721339a82d
Viktor Kozma <kozmaviktor1992@gmail.com>
2022-08-18 14:39:47 +0800

    Created the models

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  before_destroy :check_for_orders
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+
+  private
+
+  def check_for_orders
+    if bookmarks.count > 0
+      return false
+    end
+  end
+
+end
